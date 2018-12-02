70c54a5b39ef2bdc5d7afeb8c27e9af03ef23781
David <davidzapata56@gmail.com>
2018-12-02 11:11:48 -0500

    change

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -10,11 +10,16 @@ class RoomsController < ApplicationController
   def create
     @room = Room.new(room_params)
     if @room.save
+      flash[:notice] = "Post successfully created"
       redirect_to rooms_path
     else
+      flash[:alert] = @room.errors.full_messages.join(", ")
       render :new
     end
   end
+
+
+  
 
   def destroy
     room = Room.find(params[:id])
@@ -39,8 +44,10 @@ def update
 end
 
 
-  protected
-    def room_params
-      params.require(:room).permit(:title, :description, :beds, :guests, :image_url)
-    end
+
+protected
+def room_params
+  params.require(:room).permit(:title, :description, :beds, :guests, :image_url, :price_per_night)
+end
+
 end
